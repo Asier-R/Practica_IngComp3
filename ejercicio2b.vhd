c3c3fa2bc4ba38cd8a053a75771e35a9cffdaa92
Asier <monstruosaurio@gmail.com>
2023-04-06 12:43:57 +0200

    Inicio ejercicio2b

---
+++ ejercicio2b.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity ejercicio2b is port
+	(y    : out signed (7 downto 0);
+	 a, b : in  signed (7 downto 0);
+	 ctrl : in  bit);
+end entity ejercicio2b;
+
+architecture ejercicio2b of ejercicio2b is
+begin
+	
+-- asignaciones concurrentes condicionales pag 107
+
+
+
+end architecture ejercicio2b;
